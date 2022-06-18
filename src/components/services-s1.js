d83d9a486fe0fa64bcf6ae3aff08a5e6023d1cc3
Tm <pankowski.email@gmail.com>
2022-06-18 09:08:06 +0200

    frontend

---
+++ src/components/services-s1.js
@@ -0,0 +1,29 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import Row from "../../node_modules/react-bootstrap/Row";
+import Col from "../../node_modules/react-bootstrap/Col";
+
+class ServicesS1 extends Component{
+    render(){
+        return(    
+            <Container fluid className="services-s1 d-flex minh-50vh align-items-center py-5">
+                <Row className="mx-0 w-100 mt-5">
+                    <Col xs={12} md={5} className="mx-auto d-flex align-items-center">
+                        <div className="w-100 text-center pt-5 text-shadow">
+                            <h2 className="dispaly-6 fw-bold text-white">
+                                Dolor sit amet         
+                            </h2>
+                            <p className="text-white">
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna 
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default ServicesS1;
--- src/views/faq.js
+++ src/views/faq.js
@@ -1,11 +1,14 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import FaqS1 from "../components/faq-s1";
+import FaqS2 from "../components/faq-s2";
 
 class Faq extends Component{
     render(){
         return(        
-            <Container fluid className="faq-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <FaqS1/>
+                <FaqS2/>
             </Container>    
         );
     }
